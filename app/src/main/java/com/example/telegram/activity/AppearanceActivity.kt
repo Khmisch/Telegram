d106d7d3205c51c241323134ebf0fc5781fce09a
Mukaddam Khamraeva <khamraeva.m1124@gmail.com>
2022-02-09 18:34:37 +0500

    commit

--- app/src/main/java/com/example/telegram/activity/AppearanceActivity.kt
+++ app/src/main/java/com/example/telegram/activity/AppearanceActivity.kt
@@ -1,43 +1,53 @@
 package com.example.telegram.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.NestedScrollView
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.telegram.R
 import com.example.telegram.adapter.AppearenceAdapter
 import com.example.telegram.adapter.AppearenceAdapterAppIcon
 import com.example.telegram.model.Chat
-import java.util.ArrayList
-import androidx.recyclerview.widget.LinearLayoutManager
-
-
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
 
 
 class AppearanceActivity : AppCompatActivity() {
 
     lateinit var nestedScrollView: NestedScrollView
     lateinit var recyclerView: RecyclerView
+
     lateinit var recyclerView_app: RecyclerView
+     val appBarLayout: AppBarLayout = TODO()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_appearance)
         initViews()
 
     }
-        private fun initViews() {
-            nestedScrollView = findViewById(R.id.nestedScrollView)
-            recyclerView = findViewById(R.id.recyclerView)
-            recyclerView_app = findViewById(R.id.recyclerView_app)
-            val horizontalLayoutManagaer = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-            recyclerView.layoutManager = horizontalLayoutManagaer
-            refreshAdapter(getAllIcon())
-            val horizontalLayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-            recyclerView_app.layoutManager = horizontalLayoutManager
-            refreshAdapterApp(getAllChats())
-        }
+
+
+
+    private fun initViews() {
+
+
+        nestedScrollView = findViewById(R.id.nestedScrollView)
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView_app = findViewById(R.id.recyclerView_app)
+        val horizontalLayoutManagaer =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager = horizontalLayoutManagaer
+        refreshAdapter(getAllIcon())
+        val horizontalLayoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView_app.layoutManager = horizontalLayoutManager
+        refreshAdapterApp(getAllChats())
+    }
 
     private fun refreshAdapterApp(chats: ArrayList<Chat>) {
         recyclerView.isNestedScrollingEnabled = false
@@ -50,32 +60,34 @@ class AppearanceActivity : AppCompatActivity() {
         recyclerView_app!!.adapter = adapter_app
 
 
-        }
+    }
 
-        private fun getAllChats(): ArrayList<Chat> {
-            val chats: ArrayList<Chat> = ArrayList<Chat>()
-            for (i in 1..6) {
-                if (i == 1 ) {
-                    chats.add(Chat(R.drawable.ic_theme, "","", true))
-                }else if (i == 2 ) {
-                    chats.add(Chat(R.drawable.ic_theme2, "","",false ))
-                }else if (i == 3 ) {
-                    chats.add(Chat(R.drawable.ic_theme4, "","",false ))
-                }else if (i == 4 ) {
-                    chats.add(Chat(R.drawable.ic_theme3, "","",false ))
-                }else if (i == 5 ) {
-                    chats.add(Chat(R.drawable.ic_theme5, "","",false ))
-                } else {
-                    chats.add(Chat(R.drawable.ic_theme6,  "","",false))
-                }
+    private fun getAllChats(): ArrayList<Chat> {
+        val chats: ArrayList<Chat> = ArrayList<Chat>()
+        for (i in 1..6) {
+            if (i == 1) {
+                chats.add(Chat(R.drawable.ic_theme, "", "", true))
+            } else if (i == 2) {
+                chats.add(Chat(R.drawable.ic_theme2, "", "", false))
+            } else if (i == 3) {
+                chats.add(Chat(R.drawable.ic_theme3, "", "", false))
+            } else if (i == 4) {
+                chats.add(Chat(R.drawable.ic_theme4, "", "", false))
+            } else if (i == 5) {
+                chats.add(Chat(R.drawable.ic_theme5, "", "", false))
+            } else {
+                chats.add(Chat(R.drawable.ic_theme6, "", "", false))
             }
-            return chats
         }
+        return chats
+    }
+
     private fun getAllIcon(): ArrayList<Chat> {
-            val chats: ArrayList<Chat> = ArrayList<Chat>()
-            for (i in 1..5) {
-                chats.add(Chat(R.drawable.ic_telegram, "", "", false))
-            }
-            return chats
+        val chats: ArrayList<Chat> = ArrayList<Chat>()
+        for (i in 1..5) {
+            chats.add(Chat(R.drawable.ic_telegram, "", "", false))
         }
+        return chats
+    }
+
 }
--- app/src/main/java/com/example/telegram/fragment/SettingsFragment.kt
+++ app/src/main/java/com/example/telegram/fragment/SettingsFragment.kt
@@ -2,19 +2,23 @@ package com.example.telegram.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.telegram.R
 import com.example.telegram.activity.AppearanceActivity
 import com.example.telegram.activity.NotificaionActivity
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
+
 
 class SettingsFragment : Fragment() {
     lateinit var tv_notification: TextView
+    lateinit var imageView: ImageView
     lateinit var tv_appearance: TextView
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -23,7 +27,36 @@ class SettingsFragment : Fragment() {
         return view
     }
 
+    private enum class State {
+        EXPANDED, COLLAPSED, IDLE
+    }
     private fun initViews(view: View) {
+        val TAG = "AppBarTest"
+        val mAppBarLayout: AppBarLayout = view.findViewById(R.id.appbar)
+        mAppBarLayout.addOnOffsetChangedListener(object : OnOffsetChangedListener {
+            private var state: State? = null
+            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                imageView = view.findViewById(R.id.ic_search)
+                state = if (verticalOffset == 0) {
+                    if (state !== State.EXPANDED) {
+                        Log.d(TAG, "Expanded")
+                        imageView.visibility = View.INVISIBLE
+                    }
+                    State.EXPANDED
+                } else if (Math.abs(verticalOffset) >= appBarLayout.totalScrollRange) {
+                    if (state !== State.COLLAPSED) {
+                        Log.d(TAG, "Collapsed")
+                        imageView.visibility = View.VISIBLE
+                    }
+                    State.COLLAPSED
+                } else {
+                    if (state !== State.IDLE) {
+                        Log.d(TAG, "Idle")
+                    }
+                    State.IDLE
+                }
+            }
+        })
         tv_notification = view.findViewById(R.id.tv_notification)
         tv_appearance = view.findViewById(R.id.tv_appearance)
 
