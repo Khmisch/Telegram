639fcf021d9af8df1b7d49f75f173f91fc9d5089
Mukaddam Khamraeva <khamraeva.m1124@gmail.com>
2022-02-09 16:13:26 +0500

    commit

--- app/src/main/java/com/example/telegram/activity/MainActivity.kt
+++ app/src/main/java/com/example/telegram/activity/MainActivity.kt
@@ -1,25 +1,13 @@
 package com.example.telegram.activity
 
-import android.media.audiofx.Equalizer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager.widget.ViewPager
 import com.example.telegram.R
-import com.example.telegram.adapter.ChatAdapter
-import com.example.telegram.adapter.CustomAdapter
 import com.example.telegram.fragment.ChatFragment
 import com.example.telegram.fragment.ContactsFragment
 import com.example.telegram.fragment.SettingsFragment
-import com.example.telegram.model.Chat
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.tabs.TabLayout
-import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
--- app/src/main/java/com/example/telegram/adapter/SettingsAdapter.kt
+++
@@ -1,4 +0,0 @@
-package com.example.telegram.adapter
-
-class SettingsAdapter {
-}
